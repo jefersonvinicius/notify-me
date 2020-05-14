b68aaff5e80d27226c3ded0ea37ecbc82a67d80e
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2020-05-13 21:17:37 -0300

    Config react-native-push-notification lib

--- App.tsx
+++ App.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useEffect} from 'react';
+import {View, StyleSheet, Button} from 'react-native';
+
+import PushNotification from 'react-native-push-notification';
 
 export default function App() {
+  useEffect(() => {
+    PushNotification.configure({
+      onNotification: (notification) => {
+        console.log(notification);
+      },
+      requestPermissions: false,
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Notify Me</Text>
+      <Button
+        title="Notify me"
+        onPress={() => {
+          PushNotification.localNotificationSchedule({
+            message: 'Teste',
+            date: new Date(Date.now() + 5000),
+          });
+        }}
+      />
     </View>
   );
 }
